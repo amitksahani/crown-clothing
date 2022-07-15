62bded88908f45120a8b2981e425e2cf495c3de5
Amit Kumar Sahani <amit.sahani@publicissapient.com>
2022-07-15 16:56:10 +0530

    added signin and firebase

---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,13 @@
+import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
+
+const SignIn = () =>{
+    const logGoogleUser = async ()=>{
+        const {user} = await signInWithGooglePopup()
+        const userDocRef = await createUserDocumentFromAuth(user)
+    }
+    return <div>
+        <button onClick={logGoogleUser}>Sign In Page</button>
+    </div>
+}
+
+export default SignIn
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,46 @@
+import {initializeApp} from 'firebase/app'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import {doc, getDoc, getFirestore, setDoc} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBOZrcJ-mF0na-pnxE48C4Ptn_PuJrYyPE",
+    authDomain: "crown-clothing-db-ba8b7.firebaseapp.com",
+    projectId: "crown-clothing-db-ba8b7",
+    storageBucket: "crown-clothing-db-ba8b7.appspot.com",
+    messagingSenderId: "217896776804",
+    appId: "1:217896776804:web:62b5fa24231f1752525b71"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  export const provider = new GoogleAuthProvider()
+  provider.setCustomParameters({
+    prompt: 'select_account'
+  })
+
+  export const auth = getAuth()
+  export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+  export const db = getFirestore()
+  export const createUserDocumentFromAuth = async (userAuth)=>{
+    const userDocRef = doc(db, 'user', userAuth.uid)
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot.exists())
+
+    if(!userSnapshot.exists()){
+      const {displayName, email} = userAuth
+      const createdAt = new Date()
+      try {
+        await setDoc(userDocRef,{
+          displayName,
+          email,
+          createdAt
+        } )
+      } catch (error) {
+        console.log('Error', error.message)
+      }
+    }
+    return userDocRef;
+  }
